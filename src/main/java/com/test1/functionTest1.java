ff89ac17bb042e77e764b629f671d34898f13ea6
明明 <oneday2321@163.com>
2022-09-11 14:37:34 +0800

    登录-2

--- src/main/java/com/test1/functionTest1.java
+++ src/main/java/com/test1/functionTest1.java
@@ -1,5 +1,6 @@
 package com.test1;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class functionTest1 {
@@ -223,9 +224,41 @@ public class functionTest1 {
         return 1;
     }
 
-
-
-
-
-
+    public static String yanZ(){
+        /*
+        * @Param 无参
+        * @Return 返回生成的验证码
+        */
+        char[] cha = new char[5];
+        char[] zifu = { 'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m',
+                'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M',
+                '0','1','2','3','4','5','6','7','8','9'};
+        Random random = new Random();
+        while (true) {
+            for (int i1 = 0; i1 < cha.length; i1++) {
+                int i = random.nextInt(62);
+                cha[i1] = zifu[i];
+            }
+            int num = 5;
+            for (char value : cha) {
+                if (value >= '0' && value <= '9') {
+                    num -= 1;
+                }
+            }
+            if(num < 4){
+                continue ;
+            }
+            int num1 = 0;
+            for (char c : cha) {
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                    num1 += 1;
+                }
+            }
+            if (num1 >4){
+                continue ;
+            }
+            break ;
+        }
+        return new String(cha);
+    }
 }
